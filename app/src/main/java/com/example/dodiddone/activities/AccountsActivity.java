0e047ddd3a9fe6b46de7a2ceb6349029c2547be6
Matthieu Herchy <matthieu.herchy@enedis-grdf.fr>
2020-06-06 22:00:08 +0200

    token request on login

--- app/src/main/java/com/example/dodiddone/activities/AccountsActivity.java
+++ app/src/main/java/com/example/dodiddone/activities/AccountsActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.EditText;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.dodiddone.R;
-import com.example.dodiddone.services.RequestManager;
 
-import org.json.JSONObject;
+import com.example.dodiddone.R;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 
 public class AccountsActivity extends AppCompatActivity {
 
@@ -29,8 +33,33 @@ public class AccountsActivity extends AppCompatActivity {
     public void onClickAuth(View view) {
         String mail = ((EditText)findViewById(R.id.auth_mail)).getText().toString();
         String pwd = ((EditText)findViewById(R.id.auth_pwd)).getText().toString();
-        String authToken = "0123456789";
-        createAccount(mail,pwd,authToken);
+        request(mail, pwd);
+    }
+
+
+    private void request(final String email, final String password) {
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                URL url = null;
+                HttpURLConnection urlConnection = null;
+                InputStreamReader reader = null;
+                try {
+                    url = new URL("http://10.0.2.2:8008/login");
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    reader = new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8);
+                    JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+                    String token = json.get("token").getAsString();
+                    createAccount(email, password, token);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            }
+        }).start();
+
     }
 
     public void createAccount(String email, String password, String authToken) {
@@ -40,35 +69,10 @@ public class AccountsActivity extends AppCompatActivity {
         am.addAccountExplicitly(account, password, null);
         am.setAuthToken(account, "full_access", authToken);
 
-        request(email, password);
-
         finish();
     }
 
-    private void request(String email, String password) {
-        String url = "http://10.0.2.2:8008/login";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.i("Login request", response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
 
-                    }
-                });
-
-        // Access the RequestQueue through your singleton class.
-        RequestManager.getInstance(this).addToRequestQueue(jsonObjectRequest);
-
-    }
 }
